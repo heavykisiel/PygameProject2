96772c1131d1c12dd116b1f6ca56701ce5d22e9f
fmpara <95306215+fmpara@users.noreply.github.com>
2022-10-31 12:14:52 +0100

    Add files via upload

--- Bullets.py
+++ Bullets.py
@@ -1,14 +1,14 @@
 import pygame
 from textures import TextureLoader
 
-
 class Bullets(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, direction, speed):
+    def __init__(self, x, y, scale, direction, speed,surface_size):
         pygame.sprite.Sprite.__init__(self)
         image = TextureLoader.Load_Bullet_test_Texture()
         self.image = pygame.transform.scale(image, (int(image.get_width() * scale), int(image.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        self.surface_size = surface_size
         self.speed = speed
         self.direction = direction
         self.speedBullet = 2
@@ -19,3 +19,8 @@ class Bullets(pygame.sprite.Sprite):
             self.rect.x += (self.direction * self.speed)
         if self.direction == 2 or self.direction == -2:
             self.rect.y += ((self.direction / 2) * self.speed)
+        
+        if self.rect.left<15 or self.rect.right > self.surface_size[0]-15:
+            self.kill()
+    
+            
