d06fa5cd248c86429139b7889f30a21568fe46c0
heavykisiel <nikolas99@wp.pl>
2022-10-31 12:17:53 +0100

    trying new map

--- map/Map.py
+++ map/Map.py
@@ -1,19 +1,51 @@
-
+import random
 class Map:
     def __init__(self):
-        self.ChunksX = 2  # 64
-        self.ChunksY = 2  # 64
+        self.ChunksX = 8  # 64
+        self.ChunksY = 8  # 64
 
         self.ChunkMap = [[[0, 0, 0] for x in range(self.ChunksX)] for y in range(self.ChunksY)]
         self.mapPrinter()
 
     def mapPrinter(self):
 
-        for enumi, i in enumerate(self.ChunkMap):
-            for enumj, j in enumerate(i):
-                self.ChunkMap[enumi][enumj] = [enumi, enumj, 0]
+        mazeStart_X = random.randint(1, self.ChunksX - 2)
+        mazeStart_Y = random.randint(1, self.ChunksY - 2)
+        wall_types = ['w', 'n', 's', 'e']
+        walls = []
 
+        for enumx, x in enumerate(self.ChunkMap):
+            for enumy, y in enumerate(x):
+                self.ChunkMap[enumx][enumy] = [enumx, enumy, 0]
+        self.ChunkMap[mazeStart_X][mazeStart_Y] = [mazeStart_X, mazeStart_Y, 'wnse']
+        walls.append([mazeStart_X - 1, mazeStart_Y])    # wall west
+        walls.append([mazeStart_X, mazeStart_Y - 1])    # wall north
+        walls.append([mazeStart_X, mazeStart_Y + 1])    # wall south
+        walls.append([mazeStart_X + 1, mazeStart_Y])    # wall east
+
+        # current_cell = self.ChunkMap[mazeStart_X][mazeStart_Y]
+        # x = random.randint(0, 3)
+        # print(has_walls(current_cell))
+        # # if x == 0:
+        # #     wall_types[0]
+        # #     if current_cell[2].:
+        #
+        # print(mazeStart_X, mazeStart_Y)
+        # for x in self.ChunkMap:
+        #     print(x)
         # self.ChunkMap[1][1] = [1, 1, 1]
 
+def has_walls(cell, wall):
+    str_wall = ""
+    for char in cell:
+        if char == 'w':
+            str_wall += 'w'
+        if char == 'n':
+            str_wall += 'n'
+        if char == 's':
+            str_wall += 's'
+        if char == 'e':
+            str_wall += 'e'
+    return str_wall
 
 # ChunkSize = {'x': 8, 'y': 8}
